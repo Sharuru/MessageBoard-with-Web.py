f194d17963045c25d7a2c6a0b9533ec6eba36e99
Mave <i@0w0.re>
2014-07-28 20:04:24 +0800

    Temporary Backup.
    New: Data validation check.

--- Web.py Play/code.py
+++ Web.py Play/code.py
@@ -1,5 +1,5 @@
 ﻿import web
-import time
+import datetime
 
 render = web.template.render('templates/')
 
@@ -16,21 +16,32 @@ db = web.database(dbn='sqlite', db = 'MessageRecord.db')
 
 #form表单
 ##新留言表单
+vemail = web.form.regexp(r".*@.*", "Must be a VALID E-mail address!")
+
 newPostForm = web.form.Form(
-                            web.form.Textbox('Your Name'),
-                            web.form.Textarea('Message'),
+                            web.form.Textbox('username', description = 'Your Name:'),
+                            web.form.Textbox('mail', vemail, description = 'E-mail:'),
+                            web.form.Textarea('message', description = 'Message:'),
                             web.form.Button('Post it!')
 )
 
 class IndexHandler:
     def GET(self):
         msgs = db.select('msg')
-        return render.index(msgs)
+        form = newPostForm()
+        return render.index(msgs, form)
 
 class NewPostHandler:
     def POST(self):
         form = newPostForm()
-        return render.newPost(form)
+        recvTime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        if not form.validates():
+            return render.newPost(form, False, recvTime)
+        else:
+            return render.newPost(form, True, recvTime)
+            #return render.newPost(form)
+            #return web.seeother('/')
+            #return render.newPost(web.form.username)
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
