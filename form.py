e0b288b22a2de17202d796386d91787e3c055557
Mave <i@0w0.re>
2014-08-06 14:54:15 +0800

    Init form.py

---
+++ form.py
@@ -0,0 +1,30 @@
+__author__ = 'Mave'
+
+from web import form
+
+#All form in system should be set here
+
+##NewPostForm
+email_verify = form.regexp(r".*@.*", "Must be a VALID E-mail address!")
+
+newPostForm = form.Form(
+    form.Textbox('username', description='Your Name:'),
+    form.Textbox('mail', email_verify, description='E-mail:'),
+    form.Textarea('message', description='Message:'),
+    form.Button('Post it!')
+)
+
+##DelPostForm
+delPostForm = form.Form(
+    form.Button('Delete This Message')
+)
+
+##LogInForm
+admin_name_verify = form.regexp(r"\w{1,}", "Must Fill Your Account")
+admin_pass_verify = form.regexp(r"\w{1,}", "Must Fill Your Password")
+
+logInForm = form.Form(
+    form.Textbox('admin_name', admin_name_verify, description='Administrator:'),
+    form.Password('admin_pass', admin_pass_verify, description='Password:'),
+    form.Button('login', html='Login'),
+)
