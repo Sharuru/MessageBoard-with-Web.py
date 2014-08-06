a843e7676cbe8f13ce3455a3cc920aae6998612e
ricterz <ricterzheng@gmail.com>
2014-08-06 15:51:27 +0800

    urls

---
+++ urls.py
@@ -0,0 +1,10 @@
+__author__ = 'Mave'
+#All URL JUMP in system should be set here
+
+urls = (
+    '/', 'IndexHandler',
+    '/newPost', 'NewPostHandler',
+    '/delPost', 'DeletePostHandler',
+    '/logIn', 'LogInHandler'
+)
+
